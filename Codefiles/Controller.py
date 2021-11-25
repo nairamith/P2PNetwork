597b6f2679c50096bdb30280b5af6e197b251e99
amithnair <amithnair2@gmail.com>
2021-11-25 16:37:19 +0000

    Adding peer to peer communication

--- Codefiles/Controller.py
+++ Codefiles/Controller.py
@@ -8,7 +8,7 @@ app = socketio.WSGIApp(sio_server, static_files={
 })
 
 node_list = [{'id': 'c1', 'supernode': 3, 'purpose': 'F'}]
-super_node_list = [{'supernode': 'c1', 'count': 3, 'purpose': 'F'}, {'supernode': 'c2', 'count': 3, 'purpose': 'F'}]
+super_node_list = [{'supernode': 'c1', 'count': 3, 'purpose': 'F'}]
 cluster_max_count = 5
 
 
@@ -36,55 +36,52 @@ def get_cluster_list_with_purpose(purpose):
 def get_vacant_supernode(id, purpose):
     vacant_list = get_cluster_list_with_purpose(purpose)
     if len(vacant_list) == 0:
-        return id
+        return {'supernode': id, 'count': 1, 'purpose': purpose}
     else:
         return get_cluster_list_with_purpose(purpose)[0]
 
 
-# Accept/Reject registration
-# ToDo: Conditional registration based on availability in cluster
-@sio_server.event
-def supernode_registration(sid, data):
-    add_cluster(data["ID"], 1, data["purpose"])
-    print(super_node_list)
-    sio_client.connect(data["ID"])
-    sio_client.emit("registration_response", {"is_super_node": True})
-    sio_client.disconnect()
+# # Accept/Reject registration
+# #
+# @sio_server.event
+# def supernode_registration(sid, data):
+#     add_cluster(data["ID"], 1, data["purpose"])
+#     print(super_node_list)
+#     sio_client.connect(data["ID"])
+#     sio_client.emit("registration_response", {"is_super_node": True})
+#     sio_client.disconnect()
 
 
+# Connection Message
 @sio_server.event
 def connect(sid, environ):
     print('connect ', sid)
 
 
-@sio_server.event
-def SensorReading(sid, data):
-    print('message ', data, sid)
+# # Respond to supernodes list request from Nodes
+# @sio_server.event
+# def request_supernodes(sid, data):
+#     print(data)
+#     print(get_cluster_list_with_purpose("F"))
+#     sio_client.connect(data["id"])
+#     sio_client.emit("cluster_list", get_cluster_list_with_purpose(data))
+#     sio_client.disconnect()
 
 
-# Respond to supernodes list request from Nodes
-@sio_server.event
-def request_supernodes(sid, data):
-    print(data)
-    print(get_cluster_list_with_purpose("F"))
-    sio_client.connect('http://localhost:5001')
-    sio_client.emit("cluster_list", get_cluster_list_with_purpose(data))
-    sio_client.disconnect()
-
-
+# Disconnection Message
 @sio_server.event
 def disconnect(sid):
     print('disconnect ', sid)
 
 
-####################################
+#  Response to the register request sent by the nodes
 @sio_server.event
 def register(sid, data):
     print("=======================================================")
     print("Received registration request from ", data["id"])
     print("=======================================================")
     supernode = get_vacant_supernode(data["id"], data["purpose"])["supernode"]
-    add_node(data["id"], purpose=data["purpose"], supernode = supernode)
+    add_node(data["id"], purpose=data["purpose"], supernode=supernode)
     is_node_super = 0
     if supernode == data["id"]:
         is_node_super = 1
@@ -92,9 +89,10 @@ def register(sid, data):
     else:
         add_node_to_cluster(supernode, 1)
 
-    sio_client.connect('http://localhost:5001')
+    sio_client.connect(data["id"])
     sio_client.emit("cluster_info", {"supernode": supernode, "is_super" : is_node_super})
-    # sio_client.disconnect()
+    sio_client.sleep(1)
+    sio_client.disconnect()
 
 
 if __name__ == '__main__':
--- Codefiles/Node1.py
+++ Codefiles/Node1.py
@@ -8,8 +8,9 @@ IP = "localhost"
 thread = Thread()
 thread_stop_event = Event()
 
+thread_sensor = Thread()
 
-purpose = "F"
+purpose = "L"
 is_super = 0
 supernode = ""
 port = 5001
@@ -27,11 +28,12 @@ app = socketio.WSGIApp(sio_server, static_files={
 @sio_client.event
 def connect():
     print('connection established')
-    # sio.start_background_task(sendMessage())
+
 
 @sio_client.event
 def disconnect():
     print('disconnected from server')
+
 
 # def sendMessage():
 #     while 1:
@@ -40,46 +42,50 @@ def disconnect():
 #         sio_client.sleep(5)
 
 
-# Request to ask from controller for list of supernodes
-def request_supernodes():
-    sio_client.connect('http://localhost:5000')
-    sio_client.emit('request_supernodes', purpose)
-    sio_client.disconnect()
+# # Request to ask from controller for list of supernodes
+# def request_supernodes():
+#     sio_client.connect('http://localhost:5000')
+#     sio_client.emit('request_supernodes', purpose)
+#     sio_client.disconnect()
+#
+#
+# # The cluster list as response from controller. A response to the request request_supernodes
+# @sio_server.event
+# def cluster_list(sid, data):
+#     print("Received ", data, "from ", sid)
+#
+#
+# # Request to register as supernode
+# def register_supernode():
+#     print("IS socket connected", sio_client.connected)
+#     sio_client.connect('http://localhost:5000')
+#     sio_client.emit('supernode_registration', {'ID': host, 'purpose': purpose})
 
 
-# The cluster list as response from controller. A response to the request request_supernodes
-@sio_server.event
-def cluster_list(sid, data):
-    print("Received ", data, "from ", sid)
+# # Response to supernode registration
+# @sio_server.event
+# def registration_response(sid, data):
+#     is_super = data["is_super_node"]
+#     print("I am Super")
 
 
-# Request to register as supernode
-def register_supernode():
-    print("IS socket connected", sio_client.connected)
-    sio_client.connect('http://localhost:5000')
-    sio_client.emit('supernode_registration', {'ID': host, 'purpose': purpose})
-
-
-# Response to supernode registration
-@sio_server.event
-def registration_response(sid, data):
-    is_super = data["is_super_node"]
-    print("I am Super")
-
-
-
-# Request to ask from controller for list of supernodes
+# Request to register
 def register():
     print("=======================================================")
     print("Registering on controller")
     print("=======================================================")
     sio_client.connect('http://localhost:5000')
     sio_client.emit('register', {"purpose": purpose, "id": host})
+    sio_client.sleep(1)
+    sio_client.disconnect()
     # sio_client.disconnect()
 
 
+# Response from the controller for the registration
 @sio_server.event
 def cluster_info(sid, data):
+    global is_super
+    global supernode
     is_super = data["is_super"]
     supernode = data["supernode"]
     print("=======================================================")
@@ -87,11 +93,34 @@ def cluster_info(sid, data):
     print("=======================================================")
 
 
+@sio_server.event
+def SensorReading(sid, data):
+    print('message ', data, sid)
 
 
+# Function to start listening on the given post
 def serve_app(_sio, _app):
     app = socketio.Middleware(_sio, _app)
     eventlet.wsgi.server(eventlet.listen(('', port)), app)
+
+
+# Function to Emit sensor details to super node
+def send_message():
+    print("Sending sensor info To SuperNode")
+    while 1:
+        sio_client.emit('SensorReading', {'speed': str(random.random())})
+        sio_client.sleep(1)
+
+
+# Function to start a thread to start sending message
+def send_sensor_info():
+    print("=======================================================")
+    print("Sending Sensor info to supernode", supernode)
+    print("=======================================================")
+    sio_client.connect(supernode)
+    thread_sensor = sio_client.start_background_task(send_message())
+    thread_sensor.daemon = True
+    thread_sensor.start()
 
 
 thread = Thread(target=serve_app, args=(sio_server, app))
@@ -100,3 +129,12 @@ thread.start()
 # request_supernodes()
 # register_supernode()
 register()
+if not is_super:
+    send_sensor_info()
+
+while 1:
+    a=2
+
+
+
+
